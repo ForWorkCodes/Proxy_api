7bc46cf12b3951bb9e4a8b9445048c53f3d06456
Anton <ForWorkCodes@gmail.com>
2025-12-29 19:13:13 +0000

    log

--- app/jobs/notification_checker.py
+++ app/jobs/notification_checker.py
@@ -3,7 +3,7 @@ import asyncio
 from app.services.notification_service import NotificationService
 from app.core.db import get_async_session
 from datetime import datetime, timezone
-from app.core.logging_config import setup_logging
+from app.core.logging_config import setup_logging, log_to_file
 import logging
 
 app = typer.Typer()
@@ -25,6 +25,14 @@ def check_expired():
         session = await session_gen.__anext__()
 
         try:
+            now = datetime.now(timezone.utc).replace(tzinfo=None)
+
+            log_to_file(
+                "notification_checker_debug.log",
+                "[CRON DEBUG] Start check at %s",
+                now.isoformat(),
+                level=logging.DEBUG,
+            )
             not_service = NotificationService(session)
             now = datetime.now(timezone.utc).replace(tzinfo=None)
             logger.info(f"[CRON] Start check at {now.isoformat()}")
